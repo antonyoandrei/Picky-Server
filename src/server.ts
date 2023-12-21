c7117ad5083a65539dfe1064f4136d5667249e38
Antonyo Andrei <antonyoandrei7@gmail.com>
2023-12-21 17:32:17 +0100

    return server

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,6 @@ import errorHandler from './middlewares/error.middleware';
 import morgan from 'morgan';
 import FileUpload from 'express-fileupload';
 import { Request, Response } from 'express';
-import { prismaClient } from './db/client';
 
 const app = express();
 app.use(cors());
@@ -19,32 +18,13 @@ app.use(FileUpload({
     limits: {fileSize: 10000000},
     abortOnLimit: true
 }));
-// app.use("/user", userRoutes);
+app.use("/user", userRoutes);
 app.use("/movie", moviesRoutes);
 app.use("/genre", genresRoutes);
 app.use(errorHandler)
 
 app.get("/", (req: Request, res: Response): void => {
     res.status(200).json({ message: "This is working bro!" });
-});
-
-app.get("/user", (req: Request, res: Response): void => {
-    try {
-        const allUsers = prismaClient.user.findMany({
-            include: {
-                movies: {
-                    include: {
-                        genres: {
-                            select: { genre: { select: { name: true, id: true } } } 
-                        },
-                    },
-                }
-            }
-        });
-        res.status(201).json(allUsers);
-    } catch (error) {
-        res.status(200).send('Cannot find all users');
-    }
 });
 
 export default app;
