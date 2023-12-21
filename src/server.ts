30ae89c33c169c711efa08687205ac5c716f917c
Antonyo Andrei <antonyoandrei7@gmail.com>
2023-12-21 16:45:05 +0100

    update 8

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,6 @@ import errorHandler from './middlewares/error.middleware';
 import morgan from 'morgan';
 import FileUpload from 'express-fileupload';
 import { Request, Response } from 'express';
-import { prismaClient } from './db/client';
 
 const app = express();
 app.use(cors());
@@ -24,27 +23,8 @@ app.use("/movie", moviesRoutes);
 app.use("/genre", genresRoutes);
 app.use(errorHandler)
 
-app.get("/user/:userEmail", async (req: Request, res: Response) => {
-    const { userEmail } = req.params;
-
-    try {
-        const user = await prismaClient.user.findUnique({
-            where: { email: userEmail },
-            include: {
-                movies: {
-                    include: {
-                        genres: {
-                            select: { genre: { select: { name: true, id: true } } },
-                        },
-                    },
-                },
-            },
-        });
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({ error: "Internal Server Error" });
-        console.log(error);
-    }
+app.get("/", (req: Request, res: Response): void => {
+    res.status(200).json({ message: "This is working bro!" });
 });
 
 export default app;
