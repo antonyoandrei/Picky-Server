4d4f94b3c5add521706edb4b6b228440cd90eb8f
Antonyo Andrei <antonyoandrei7@gmail.com>
2023-12-22 09:29:39 +0100

    mongo fix

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,9 @@ import FileUpload from 'express-fileupload';
 import { Request, Response } from 'express';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://picky-nextjs.vercel.app'
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(FileUpload({
