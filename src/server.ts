3bbf1fcec99e848f2c1cb51585b5248f8a124a66
Antonyo Andrei <antonyoandrei7@gmail.com>
2023-12-22 09:44:04 +0100

    cors

--- src/server.ts
+++ src/server.ts
@@ -9,11 +9,7 @@ import FileUpload from 'express-fileupload';
 import { Request, Response } from 'express';
 
 const app = express();
-app.use(cors({
-    origin: 'https://picky-nextjs.vercel.app',
-    methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: 'Content-Type',
-}));
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(FileUpload({
